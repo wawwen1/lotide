117708d52830a4c7c5b23c28e122937dd2d69300
warren roque <warrenroque17@gmail.com>
2022-03-14 04:39:04 +0000

    find first key that fits callback

---
+++ findKey.js
@@ -0,0 +1,17 @@
+const findKey = (obj, callback) => {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      console.log(key);
+      return key;
+    }
+  }
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
