4205169f1e4f897347eec9f7364615229e974574
warren roque <warrenroque17@gmail.com>
2022-03-15 05:32:50 +0000

    refactor + module.exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,22 +1,7 @@
-let assertEqual = function(actual, expected) {
-  let failed = `😭😭 Assertion Failed: ${actual} !== ${expected}`;
-  let pass = `🥰🥰 Assertion Passed: ${actual} === ${expected}`;
-  if (actual === expected) {
-    console.log(pass);
-    return;
-  } else {
-    console.log(failed);
-    return;
-  }
-
-};
-
-let eqArrays = function(arr1, arr2) {       //returns true/false if arrays match or not
- 
+let eqArrays = (arr1, arr2) => {       //returns true/false if arrays match or not
   if (arr1.length !== arr2.length) {
     return false;
   }
-
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -25,13 +10,4 @@ let eqArrays = function(arr1, arr2) {       //returns true/false if arrays match
   return true;
 };
 
-assertEqual("Sushi", "Sushi");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 17);
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
