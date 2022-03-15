0c6be60a4673d724e6e5c38fa9b755223ec35f55
warren roque <warrenroque17@gmail.com>
2022-03-15 07:06:25 +0000

    add export

--- without.js
+++ without.js
@@ -1,27 +1,3 @@
-const eqArrays = function(arr1, arr2) {       //returns true/false if arrays match or not
- 
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArrayEqual = function(arr1, arr2) {       //pass/fail msg for eqArrays
-  let fail = `🤡 🤡 🤡 Assertion Failed: ${arr1} !== ${arr2}`;
-  let pass = `👍 👍 👍 Assertion Passed: ${arr1} === ${arr2}`;
-  if (eqArrays(arr1, arr2)) {
-    console.log(pass);
-  } else {
-    console.log(fail);
-  }
-};
-
 const without = (source, itemsToRemove) => {
   let filtered = [];        //new array for filtered source
   for (let ele of source) {       //loop through source
@@ -33,6 +9,4 @@ const without = (source, itemsToRemove) => {
   return filtered;
 };
 
-assertArrayEqual(without([1, 2, 3], [3]), [1, 2]);
-assertArrayEqual(without([1, 2, 3, 4, 5], [3, 4, 1]), [2, 5]);
-assertArrayEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+module.exports = without;
