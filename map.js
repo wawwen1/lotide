19ab627fcf0b8df01f12992781968340b30a6364
warren roque <warrenroque17@gmail.com>
2022-03-15 07:04:56 +0000

    add export

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = (array, callback) => {
   let results = [];
   for (let item of array) {
@@ -8,5 +6,4 @@ const map = (array, callback) => {
   return results;
 };
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
+module.exports = map;
