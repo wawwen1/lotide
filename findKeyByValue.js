ee69ed01a9899b9d4da80e919865863740adb482
warren roque <warrenroque17@gmail.com>
2022-03-15 07:03:40 +0000

    add export

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let failed = `😭 😭 Assertion Failed: ${actual} !== ${expected}`;
-  let pass = `🥰 🥰 Assertion Passed: ${actual} === ${expected}`;
-  if (actual === expected) {
-    console.log(pass);
-  } else {
-    console.log(failed);
-  }
-};
-
 const findKey = (obj, search) => {
   for (let key in obj) {        //loop through obj's keys
     if (obj[key].includes(search)) {        //if value inside key = 'search'
@@ -16,11 +6,4 @@ const findKey = (obj, search) => {
   }
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKey(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKey(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
