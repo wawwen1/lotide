4e25dafd8ee556606bc050c816f1b3e329c02135
warren roque <warrenroque17@gmail.com>
2022-03-15 06:45:43 +0000

    add mocha & chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,16 @@
 const middle = require('../middle');
 const eqArrays = require('../eqArrays');
 const assertArrayEqual = require('../assertArrayEqual');
+const assert = require('chai').assert;
 
-assertArrayEqual(middle([1]), []); // => []
-assertArrayEqual(middle([1, 2]), []); // => []
-assertArrayEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArrayEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe('#middle', () => {
+  it('returns [2] for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it('returns [] for [1]', () => {
+    assert.include(middle[1], []);
+  });
+  it('returns [3] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
