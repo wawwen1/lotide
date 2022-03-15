a9fbd46eaed8cd14832eb3f41e4cb2c0f80c4791
warren roque <warrenroque17@gmail.com>
2022-03-15 05:09:21 +0000

    added module.exports functionality

--- head.js
+++ head.js
@@ -1,20 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 let head = function(first) {
   return first[0];
 };
-
-let assertEqual = function(actual, expected) {
-  let failed = `😭😭 Assertion Failed: ${actual} !== ${expected}`;
-  let pass = `🥰🥰 Assertion Passed: ${actual} === ${expected}`;
-  if (actual === expected) {
-    console.log(pass);
-    return;
-  } else {
-    console.log(failed);
-    return;
-  }
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), 1);
