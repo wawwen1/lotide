59456edfbbbb9cc1de7b96dbe0aa8beb5d2bed17
warren roque <warrenroque17@gmail.com>
2022-03-15 07:01:07 +0000

    add export

--- countOnly.js
+++ countOnly.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let failed = `😭 😭 Assertion Failed: ${actual} !== ${expected}`;
-  let pass = `🥰 🥰 Assertion Passed: ${actual} === ${expected}`;
-  if (actual === expected) {
-    console.log(pass);
-  } else {
-    console.log(failed);
-  }
-
-};
-
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
@@ -25,21 +14,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
