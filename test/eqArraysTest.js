bde8083bae45503b82ab3bd34bfeb5275630128a
warren roque <warrenroque17@gmail.com>
2022-03-15 06:28:48 +0000

    add mocha & chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,12 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+describe('#eqArrays', () => {
+  it('returns true for [1, 2, 3], [1, 2, 3]', () => {
+    assert.strictEqual(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+  });
+  it('returns false for [1, 2, 3], [3, 2, 1]', () => {
+    assert.strictEqual(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
+  });
+});
