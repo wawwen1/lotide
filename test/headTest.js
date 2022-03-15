473721c87bb0d3875c99c43ecae0191390e90bae
warren roque <warrenroque17@gmail.com>
2022-03-15 06:02:55 +0000

    add mocha & chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,12 @@
 const head = require('../head');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(head(['nice', 'weather', 'woo']), 'nice');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
